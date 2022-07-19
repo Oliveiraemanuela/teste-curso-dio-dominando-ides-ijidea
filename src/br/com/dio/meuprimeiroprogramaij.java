7fcd0bc16993a4470b2f676df22217c852944969
Oliveiraemanuela <oliveiraemanuela1225@gmail.com>
2022-07-19 00:36:38 -0300

    first commit

---
+++ src/br/com/dio/meuprimeiroprogramaij.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class meuprimeiroprogramaij {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 3;
+        int b = 5;
+        System.out.println("Hello, word! " + (a+b));*/
+
+    }
+}
